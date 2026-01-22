d0fa0750d80620e1e08ffe0210019b7ac8e1afa5
Katia Aresti <karestig@ibm.com>
2026-01-22 10:10:27 +0100

    test 3.31

--- infinispan-client-quickstart/src/main/java/org/acme/infinispan/client/InfinispanGreetingResource.java
+++ infinispan-client-quickstart/src/main/java/org/acme/infinispan/client/InfinispanGreetingResource.java
@@ -1,29 +1,34 @@
 package org.acme.infinispan.client;
 
+import java.util.concurrent.CompletionStage;
+
 import io.quarkus.infinispan.client.Remote;
+
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+
 import org.infinispan.client.hotrod.RemoteCache;
 
 @Path("/greeting")
 public class InfinispanGreetingResource {
 
-    @Inject
-    @Remote("mycache")
-    RemoteCache<String, Greeting> cache;
+   @Inject
+   @Remote("mycache")
+   RemoteCache<String, Greeting> cache;
 
-    @POST
-    @Path("/{id}")
-    public String postGreeting(String id, Greeting greeting) {
-        cache.put(id, greeting);
-        return id;
-    }
+   @POST
+   @Path("/{id}")
+   public CompletionStage<String> postGreeting(String id, Greeting greeting) {
+      return cache.putAsync(id, greeting)
+            .thenApply(g -> "Greeting added!")
+            .exceptionally(ex -> ex.getMessage());
+   }
 
-    @GET
-    @Path("/{id}")
-    public Greeting getGreeting(String id) {
-        return cache.get(id);
-    }
+   @GET
+   @Path("/{id}")
+   public CompletionStage<Greeting> getGreeting(String id) {
+      return cache.getAsync(id);
+   }
 }
